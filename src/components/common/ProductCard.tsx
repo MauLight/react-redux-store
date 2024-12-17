47eccc44aaee0b969843cfbe713c25c2ed238efd
Mau Ulisse Luz <maulisseluz@gmail.com>
2024-12-17 14:41:05 -0300

    feat: minor fix in productcard
    don't render inner text in individual product page

--- src/components/common/ProductCard.tsx
+++ src/components/common/ProductCard.tsx
@@ -1,14 +1,17 @@
 import { useState, type ReactElement } from 'react'
-import { addItem } from '@/features/cart/cartSlice'
+import { Link, useLocation } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux'
+import { postToWishlistAsync } from '@/features/wishList/wishListSlice'
+import { addItem } from '@/features/cart/cartSlice'
+import { AppDispatch } from '@/store/store'
+
 import { toast } from 'react-toastify'
 import { ProductProps, StoreProps } from '@/utils/types'
-import { Link } from 'react-router-dom'
-import { postToWishlistAsync } from '@/features/wishList/wishListSlice'
-import { AppDispatch } from '@/store/store'
 
 export const ProductCard = ({ product }: { product: ProductProps }): ReactElement => {
   const dispatch: AppDispatch = useDispatch()
+  const { pathname } = useLocation()
+  const isCollection = !pathname.includes('product')
   const user = useSelector((state: StoreProps) => state.userAuth.user)
   const [wishListed, setWishListed] = useState<boolean>(false)
 
@@ -32,31 +35,35 @@ export const ProductCard = ({ product }: { product: ProductProps }): ReactElemen
       <img src={image} alt="product" className="w-full sm:h-full object-cover" />
 
 
-      <div className="w-full absolute bottom-5 hidden sm:flex justify-between px-5 z-10 transition-all duration-300">
-        <div className="flex flex-col">
-          <h1 aria-label={product.title} className='text-[1rem] min-[400px]:text-[22px] uppercase neue antialiazed text-[#ffffff] leading-tight'>{product.title}</h1>
-          <div className="flex gap-x-2">
-            <p className='text-[16px] uppercase neue antialiazed text-[#ffffff]'>{`${product.price}$`}</p>
-            <p className='text-[12px] uppercase neue antialiazed text-gray-100 line-through'>{`${product.fullPrice}$`}</p>
+      {
+        isCollection && (
+          <div className="w-full absolute bottom-5 flex justify-between px-5 z-10 transition-all duration-300">
+            <div className="flex flex-col">
+              <h1 aria-label={product.title} className='text-[1rem] min-[400px]:text-[22px] uppercase neue antialiazed text-[#ffffff] leading-tight'>{product.title}</h1>
+              <div className="flex gap-x-2">
+                <p className='text-[16px] uppercase neue antialiazed text-[#ffffff]'>{`${product.price}$`}</p>
+                <p className='text-[12px] uppercase neue antialiazed text-gray-100 line-through'>{`${product.fullPrice}$`}</p>
+              </div>
+            </div>
+            <div className="flex items-center gap-x-5">
+              <button aria-label='wishlist' onClick={() => { handleWishList(product.id as string) }}>
+                {
+                  wishListed ? (
+                    <i className="fa-solid fa-heart text-indigo-500"></i>
+                  )
+                    :
+                    (
+                      <i className="fa-solid fa-heart text-[#ffffff]"></i>
+                    )
+                }
+              </button>
+              <button aria-label='add to cart' onClick={handleAddItemToCart} className='h-[50px] w-[50px] antialiased rounded-full bg-gray-900 border-t border-sym_gray-300 shadow-sm shadow-sym_gray-800 bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-70 flex justify-center items-center pb-1 cursor-pointer text-[#ffffff] hover:text-indigo-500'>
+                <i className="fa-solid fa-bag-shopping"></i>
+              </button>
+            </div>
           </div>
-        </div>
-        <div className="flex items-center gap-x-5">
-          <button aria-label='wishlist' onClick={() => { handleWishList(product.id as string) }}>
-            {
-              wishListed ? (
-                <i className="fa-solid fa-heart text-indigo-500"></i>
-              )
-                :
-                (
-                  <i className="fa-solid fa-heart text-[#ffffff]"></i>
-                )
-            }
-          </button>
-          <button aria-label='add to cart' onClick={handleAddItemToCart} className='h-[50px] w-[50px] antialiased rounded-full bg-gray-900 border-t border-sym_gray-300 shadow-sm shadow-sym_gray-800 bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-70 flex justify-center items-center pb-1 cursor-pointer text-[#ffffff] hover:text-indigo-500'>
-            <i className="fa-solid fa-bag-shopping"></i>
-          </button>
-        </div>
-      </div>
+        )
+      }
 
       <Link to={`/product/${product.id}`} className="absolute top-0 left-0 w-full sm:h-full bg-[#10100e] opacity-0 group-hover:opacity-30 z-0 transition-all duration-200"></Link>
     </section>
