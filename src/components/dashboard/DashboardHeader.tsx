4b5c39202804c9bfadff95d6c27692fdee977949
Mau Ulisse Luz <maulisseluz@gmail.com>
2025-01-09 05:24:42 -0300

    feat: fix navbar position
    make it appear above sidebar

--- src/components/dashboard/DashboardHeader.tsx
+++ src/components/dashboard/DashboardHeader.tsx
@@ -3,8 +3,8 @@ import DashboardNavbar from './DashboardNavbar'
 
 function DashboardHeader(): ReactNode {
     return (
-        <header className='w-full flex flex-col items-center bg-[#ffffff] px-5 border-b border-sym_gray-100'>
-            <section className='w-full flex  gap-x-5 py-5'>
+        <header className='w-full h-[60px] flex flex-col items-center bg-[#ffffff]'>
+            <section className='w-full h-full flex  gap-x-5 py-5 px-5 bg-[#ffffff] z-50 border-b border-sym_gray-100'>
                 <h1 className='text-[1rem] text-[#10100e] uppercase max-sm:px-4'>Admin dashboard</h1>
                 <DashboardNavbar />
             </section>
