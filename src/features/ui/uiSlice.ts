c8b4c85f386f672e7786c47cc716594afd7eb0e1
Mau Ulisse Luz <maulisseluz@gmail.com>
2025-01-20 04:37:30 -0300

    feat: remove unnused methods from uiSlice

--- src/features/ui/uiSlice.ts
+++ src/features/ui/uiSlice.ts
@@ -7,28 +7,6 @@ const url = import.meta.env.VITE_BACKEND_URL
 //const user = localStorage.getItem('marketplace-user') ? JSON.parse(localStorage.getItem('marketplace-user') as string) : {}
 //const token = user.token
 
-interface homePanelProps {
-    hero: {
-        header: string
-        subHeader: string
-        compressImage: boolean
-        image: string
-    },
-    slider: {
-        savedSliders: Array<string> //Slider list names
-        sliderSpeed: number
-        currentSlider: Array<string> //Current slider contains ordered list of images
-    },
-    collection: {
-        collections: Array<string> // Collection list names
-        defaultSorting: number
-    },
-    products: {
-        itemsPerRow: number
-        defaultSorting: number //This applies to home page
-    }
-}
-
 export const getUIConfigurationAsync = createAsyncThunk(
     'ui/getUIConfiguration', async (_, { rejectWithValue }) => {
         try {
@@ -89,24 +67,6 @@ export const updateUIConfigurationAsync = createAsyncThunk(
 export const uiSlice = createSlice({
     name: 'ui',
     initialState: {
-        ui: {
-            auth: {
-                allowGoogle: false,
-                compressImages: false,
-                header: '',
-                logoUrl: '',
-                background: ''
-            },
-            home: {
-                hero: {
-                    header: '',
-                    subHeader: '',
-                    compressImage: true,
-                    image: ''
-                }
-
-            } as homePanelProps
-        },
         authIsLoading: false,
         authHasError: false,
         homeIsLoading: false,
@@ -115,53 +75,7 @@ export const uiSlice = createSlice({
         uiIsloading: false,
         uiHasError: false
     },
-    reducers: {
-        updateAuthAllowGoogle: (state, action) => {
-            state.ui.auth.allowGoogle = action.payload
-        },
-        updateAuthCompressImages: (state, action) => {
-            state.ui.auth.compressImages = action.payload
-        },
-        updateAuthLogoUrl: (state, action) => {
-            state.ui.auth.logoUrl = action.payload
-        },
-        updateAuthHeader: (state, action) => {
-            state.ui.auth.header = action.payload
-        },
-        updateAuthBackground: (state, action) => {
-            state.ui.auth.background = action.payload
-        },
-        updateHeroHeroHeader: (state, action) => {
-            state.ui.home.hero.header = action.payload
-        },
-        updateHeroSubheader: (state, action) => {
-            state.ui.home.hero.subHeader = action.payload
-        },
-        updateHeroImage: (state, action) => {
-            state.ui.home.hero.image = action.payload
-        },
-        updateSliderSavedSliders: (state, action) => {
-            state.ui.home.slider.savedSliders = action.payload
-        },
-        updateSliderSliderSpeed: (state, action) => {
-            state.ui.home.slider.sliderSpeed = action.payload
-        },
-        updateSliderCurrentSlider: (state, action) => {
-            state.ui.home.slider.currentSlider = action.payload
-        },
-        updateCollectionCollections: (state, action) => {
-            state.ui.home.collection.collections = action.payload
-        },
-        updateCollectionsDefaultSorting: (state, action) => {
-            state.ui.home.collection.defaultSorting = action.payload
-        },
-        updateProductsItemsPerRow: (state, action) => {
-            state.ui.home.products.itemsPerRow = action.payload
-        },
-        updateProdcutsDefaultSorting: (state, action) => {
-            state.ui.home.products.defaultSorting = action.payload
-        }
-    },
+    reducers: {},
     extraReducers: (builder) => {
         builder
             .addCase(
@@ -225,23 +139,6 @@ export const uiSlice = createSlice({
     }
 })
 
-export const {
-    updateAuthAllowGoogle,
-    updateAuthCompressImages,
-    updateAuthHeader,
-    updateAuthLogoUrl,
-    updateAuthBackground,
-    updateHeroHeroHeader,
-    updateHeroSubheader,
-    updateHeroImage,
-    updateSliderSavedSliders,
-    updateSliderSliderSpeed,
-    updateSliderCurrentSlider,
-    updateCollectionCollections,
-    updateCollectionsDefaultSorting,
-    updateProductsItemsPerRow,
-    updateProdcutsDefaultSorting
-} = uiSlice.actions
 const uiReducer = uiSlice.reducer
 
 export default uiReducer
