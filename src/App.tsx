a18b33ada3982289a50b54526e518a3c6c1f439a
Mau Ulisse Luz <maulisseluz@gmail.com>
2025-03-06 10:55:55 -0300

    feat: remove footer in admin view
    render only in main page

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,11 @@ import Layout from './Layout'
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 import { animatedGradientText } from './utils/styles'
+import { useLocation } from 'react-router-dom'
 
 function App() {
+
+  const { pathname } = useLocation()
 
   return (
     <>
@@ -18,9 +21,13 @@ function App() {
           theme='dark'
           limit={1}
         />
-        <div className="w-full h-[60px] flex justify-end items-center px-10 bg-[#10100e]">
-          <h1 className='text-[#fff]'>Powered by <a href='https://symetria.lat' className={`uppercase ${animatedGradientText}`}>Symetria</a></h1>
-        </div>
+        {
+          !pathname.includes('admin') && (
+            <div className="w-full h-[60px] flex justify-end items-center px-10 bg-[#10100e]">
+              <h1 className='text-[#fff]'>Powered by <a href='https://symetria.lat' className={`uppercase ${animatedGradientText}`}>Symetria</a></h1>
+            </div>
+          )
+        }
       </div>
     </>
   )
